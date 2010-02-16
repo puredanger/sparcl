a38cbfd43c23eaf9e2b258678455e97638f78799
puredanger <alexdmiller@yahoo.com>
2010-02-16 13:38:37 -0600

    Recreated the web app

--- src/sparcl.clj
+++ src/sparcl.clj
@@ -1,7 +1,9 @@
 (ns sparcl
 	(:gen-class)
 	(:use compojure)
-	(:import [com.hp.hpl.jena.query QueryExecutionFactory QueryFactory]))
+	(:import (java.io StringReader)
+			 (com.hp.hpl.jena.query QueryExecutionFactory QueryFactory)
+			 (com.hp.hpl.jena.rdf.model ModelFactory)))
 
 (defn exec-sparql
 	"Execute SPARQL query against model and return a ResultSet"
@@ -11,13 +13,60 @@
 			(. QueryFactory create sparql) 
 			model) ))
 
-(defn exec [rdf sparql]
-	(html [:html [:body [:h1 test]]])
-)
+(defn create-model [rdf]
+	(let [model (ModelFactory/createDefaultModel)]	
+		(.read model (StringReader. rdf) nil)
+		model))
+	
+(defn data-html [results]
+	(for [solution (iterator-seq results)]
+		[:tr
+			(for [var-name (iterator-seq (.varNames solution))]
+				[:td (.toString (.get solution var-name))])]))
+	
+(defn vars-html [results] 
+	[:tr
+		(for [binding (.getResultVars results)] 
+			[:th binding])])
+		
+(defn results-html [results]
+	[:table {:border "1"}
+		(vars-html results) 
+		(data-html results)])
+
+(defn exec-html [rdf sparql results]
+	(html [:html
+			[:head
+				[:title "SPARCL: SPARQL Query Tester"]]
+			[:body
+				(form-to [:post "/"]
+					[:h2 "RDF:"]
+					[:textarea {:name "rdf" :rows 10 :cols 80} rdf]
+					[:h2 "SPARQL Query:"]
+					[:textarea {:name "sparql" :rows 10 :cols 80} sparql]
+					[:p (submit-button "Execute")]
+					[:h2 "Results:"]
+					results)]]))
+
+(defn str-exists? [s] 
+	(not (or (nil? s) (= (.length s) 0))))
+
+(defn strs-exist? [strs]
+	(every? str-exists? strs))
+
+(defn self-or-empty [s]
+	(if (nil? s) "" s))
+
+(defn exec-page [rdf sparql]
+	(exec-html 	(self-or-empty rdf) 
+				(self-or-empty sparql) 
+				(if (strs-exist? [rdf sparql]) 
+					(results-html (exec-sparql sparql (create-model rdf)))
+					[:p "No query executed."])))
 			
 (defroutes sparcl
-	(GET "/" (exec nil nil))
-	(POST "/" (exec (params :rdf) (params :sparql)))
+	(GET "/" (exec-page nil nil))
+	(POST "/" (exec-page (params :rdf) (params :sparql)))
 	(ANY "*" 404))
 
 (defn -main [& args]
